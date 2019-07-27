6c6c5fd4f249d47f03ec9fb9824c81d9d773554f
Zhenhui Chen <zhenhuiccc@gmail.com>
2019-07-27 22:40:58 +0800

    Create test_multiprocess.py

---
+++ test_multiprocess.py
@@ -0,0 +1,35 @@
+import time
+import cv2
+from multiprocessing import Process, Queue
+from video_stream import VideoStream
+
+
+def read_frame(queue):
+    video_path = 'E:/Datasets/XIO/safe_detection/still_1.avi'
+    vs = VideoStream(video_path, 'test')
+    while True:
+        frame = vs.robust_read()
+        queue.put(frame)
+        print("read a frame")
+
+
+def process(queue):
+    while True:
+        frame = queue.get(True)
+        time.sleep(0.1)
+        cv2.imshow('show', frame)
+        print("show")
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    q = Queue(50)
+    pr = Process(target=read_frame, args=(q,))
+    pp = Process(target=process, args=(q,))
+
+    pr.start()
+    pp.start()
+
+    pp.join()
