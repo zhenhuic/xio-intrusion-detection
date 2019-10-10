972616a3c28dbce5bbb9b6911a028777fc2b3d04
Zhenhui Chen <zhenhuiccc@gmail.com>
2019-10-10 22:05:17 +0800

    Update run_forever.py

--- run_forever.py
+++ run_forever.py
@@ -4,24 +4,24 @@ from gui_main import gui_main
 from monitor import is_alive
 
 
-def start_detect(detection_flag):
+def subprocess_run(detection_flag: Value) -> Process:
     p = Process(target=gui_main, args=(detection_flag,))
     p.start()
     return p
 
 
 if __name__ == '__main__':
-    detection_flag = Value('i', 0)
-    p = start_detect(detection_flag)
+    detection_flag = Value('i', 0)  # variable with shared memory between multi processes
+    p = subprocess_run(detection_flag)
 
     while True:
         time.sleep(10)
         if is_alive(detection_flag):
-            print('The process is <alive>')
+            print('The detection process is <alive>')
         else:
-            print('The process is <dead>!')
-            p.terminate()
+            print('The detection process is <dead>!')
+            p.terminate()  # kill the subprocess
+            time.sleep(2)
             print('reboot')
-            time.sleep(3)
             detection_flag = Value('i', 0)
-            p = start_detect(detection_flag)
+            p = subprocess_run(detection_flag)
