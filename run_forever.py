941c8b49701b633bd58e795876ccdb2df4f9fd92
Zhenhui Chen <zhenhuiccc@gmail.com>
2019-10-15 13:03:40 +0800

    multiprocess monitor detection subprocess

---
+++ run_forever.py
@@ -0,0 +1,27 @@
+import time
+from multiprocessing import Process, Value
+from gui_main import gui_main
+from monitor import is_alive
+
+
+def subprocess_run(detection_flag: Value) -> Process:
+    p = Process(target=gui_main, args=(detection_flag,))
+    p.start()
+    return p
+
+
+if __name__ == '__main__':
+    detection_flag = Value('i', 0)  # variable with shared memory between multi processes
+    p = subprocess_run(detection_flag)
+
+    while True:
+        time.sleep(30)
+        if is_alive(detection_flag):
+            print('The detection process is <alive>')
+        else:
+            print('The detection process is <dead>!')
+            p.terminate()  # kill the subprocess
+            time.sleep(2)
+            print('reboot')
+            detection_flag = Value('i', 0)
+            p = subprocess_run(detection_flag)
